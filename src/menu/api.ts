00f7ce63e7f5ed93a32df158fa0730dc5283624d
iconsumeplutonium <uzymc242@outlook.com>
2026-02-23 02:57:16 -0800

    get rid of frontend menu caching

--- src/menu/api.ts
+++ src/menu/api.ts
@@ -80,11 +80,11 @@ export async function getAllLocationMenus(day_offset: number = 0, signal?: Abort
     const startTime = new Date();
     const dateString = new Date(Date.now() + day_offset * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
 
-    const cachedMenu = await getCachedMenu(dateString);
-    if (cachedMenu) {
-        console.log('Using cached menu for day offset:', day_offset);
-        return cachedMenu;
-    }
+    // const cachedMenu = await getCachedMenu(dateString);
+    // if (cachedMenu) {
+    //     console.log('Using cached menu for day offset:', day_offset);
+    //     return cachedMenu;
+    // }
     console.log('Date string:', dateString);
 
     const locationMenus: Record<string, Menu> = await fetch(BASE_API_URL + '/menu?dayOffset=' + day_offset, { signal }).then(async (response) => {
@@ -101,31 +101,31 @@ export async function getAllLocationMenus(day_offset: number = 0, signal?: Abort
     const endTime = new Date();
     const timeDiff = endTime.getTime() - startTime.getTime();
     console.log('Seconds to fetch all menus:', timeDiff / 1000);
-    setCachedMenu(dateString, locationMenus);
+    // setCachedMenu(dateString, locationMenus);
     return locationMenus;
 }
 
-const CACHE_EXPIRATION_TIME = 24 * 60 * 60 * 500    // 12 hours
-async function getCachedMenu(dateString: string): Promise<Record<string, Menu> | null> {
-    const cachedMenu = localStorage.getItem('cached_menu_' + dateString);
-    if (!cachedMenu) {
-        return null;
-    }
-    const parsedMenu = JSON.parse(cachedMenu);
-    const currentTime = Date.now();
-    const timeDiff = currentTime - parsedMenu.timestamp;
-    console.log('timeDiff:', timeDiff);
-    if (timeDiff > CACHE_EXPIRATION_TIME) {
-        console.log('Cached menu expired for date:', dateString);
-        localStorage.removeItem('cached_menu_' + dateString);
-        return null;
-    }
-    console.log('Using cached menu for date:', dateString);
-    return parsedMenu.data;
-}
+// const CACHE_EXPIRATION_TIME = 24 * 60 * 60 * 500    // 12 hours
+// async function getCachedMenu(dateString: string): Promise<Record<string, Menu> | null> {
+//     const cachedMenu = localStorage.getItem('cached_menu_' + dateString);
+//     if (!cachedMenu) {
+//         return null;
+//     }
+//     const parsedMenu = JSON.parse(cachedMenu);
+//     const currentTime = Date.now();
+//     const timeDiff = currentTime - parsedMenu.timestamp;
+//     console.log('timeDiff:', timeDiff);
+//     if (timeDiff > CACHE_EXPIRATION_TIME) {
+//         console.log('Cached menu expired for date:', dateString);
+//         localStorage.removeItem('cached_menu_' + dateString);
+//         return null;
+//     }
+//     console.log('Using cached menu for date:', dateString);
+//     return parsedMenu.data;
+// }
 
-async function setCachedMenu(dateString: string, menus: Record<string, Menu>) {
-    console.log('Setting cached menu for date:', dateString);
-    localStorage.setItem('cached_menu_' + dateString, JSON.stringify({data: menus, timestamp: Date.now()}));
-}
+// async function setCachedMenu(dateString: string, menus: Record<string, Menu>) {
+//     console.log('Setting cached menu for date:', dateString);
+//     localStorage.setItem('cached_menu_' + dateString, JSON.stringify({data: menus, timestamp: Date.now()}));
+// }
 
